1a1d88b4a89440c95604c9c90cac608a95bcc5b3
unknown <g00419296@atu.ie>
2024-10-04 14:29:28 +0000

    completed part 5

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,20 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//import router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 //updated app.js to call the components i created in the folder
 
 import Header from './components/header';
 import Content from './components/content';
 import Footer from './components/footer';
+import NavigationBar from './components/navbar';
+
 
 
 //called instances of each component
+//added instance of navbar
+//added the routes
+
 function App() {
   return (
     <div>
-      <Header />
-      <Content />
-      <Footer />
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/home" element={<Content />} />
+          <Route path="/read" element={<Header />} />
+          <Route path="/create" element={<Footer />} />
+        </Routes>
+        <Footer />
+      </Router>
+    
     </div>
   );
 }
