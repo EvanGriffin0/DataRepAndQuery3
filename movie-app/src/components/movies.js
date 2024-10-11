4eb62cb75a0d43980293705190bd915e3dd715cb
unknown <g00419296@atu.ie>
2024-10-11 14:48:02 +0100

    added movies component and created a data set of objects

---
+++ movie-app/src/components/movies.js
@@ -0,0 +1,13 @@
+//create movies component 
+const Movies = () => {
+
+    return(
+        <div>
+           <h3> Hello from movies component </h3>
+        </div>
+
+    )
+
+}
+
+export default Movies;
