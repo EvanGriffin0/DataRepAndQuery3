cd6aef4832650dc4fe059daa74517ecf14cac46f
unknown <g00419296@atu.ie>
2024-10-11 14:58:08 +0100

    Added movie method that console logs the array of objects

--- movie-app/src/components/movies.js
+++ movie-app/src/components/movies.js
@@ -1,13 +1,16 @@
 //create movies component 
-const Movies = () => {
+const Movies = (props) => {
 
+    //returned a list of movies from from the array of objects found in read
     return(
         <div>
            <h3> Hello from movies component </h3>
+           {console.log(props.myMovies)}
         </div>
 
     )
 
 }
 
+//exported movies class
 export default Movies;
